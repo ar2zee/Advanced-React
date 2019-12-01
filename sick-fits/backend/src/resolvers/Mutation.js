8cc54337d9c10a3691b70b7bb8797c2caecc859e
ar2zee <arturris@mail.ru>
2019-12-01 15:09:15 -0600

    boilerplate for backend with prisma and graphql

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // Check if they are logged in
+    console.log({ ctx: ctx.db });
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    return item;
+  }
+};
 
-module.exports = mutations;
+module.exports = Mutations;
